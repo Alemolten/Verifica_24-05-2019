6c10226b85a1fcaee77d451a78d8b87696ec7d69
molteni_alessandro <molteni_alessandro@INF1-21.jeanmonnet.local>
2019-05-24 10:38:49 +0200

    realizzazione main

--- Ese1/src/ese1/Ese1.java
+++ Ese1/src/ese1/Ese1.java
@@ -5,6 +5,8 @@
  */
 package ese1;
 
+import java.util.*;
+
 /**
  *
  * @author molteni_alessandro
@@ -16,6 +18,21 @@ public class Ese1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        DatiCondivisi dati = new DatiCondivisi();
+        ThRazzo1 thRa1 = new ThRazzo1(dati);
+        ThRazzo2 thRa2 = new ThRazzo2(dati);
+        ThRoccia1 thRo1 = new ThRoccia1(dati);
+        ThRoccia2 thRo2 = new ThRoccia2(dati);
+        
+        Scanner input = new Scanner(System.in);
+        String lettera = input.next();
+        
+        if(lettera.equals("S")) {
+            thRa1.start();
+            thRa2.start();
+            thRo1.start();
+            thRo2.start();
+        }
     }
     
 }
