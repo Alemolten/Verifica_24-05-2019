835b230d5e20e372f212902938e656c4dcded665
molteni_alessandro <molteni_alessandro@INF1-21.jeanmonnet.local>
2019-05-24 10:46:17 +0200

    realizzazione DatiCondivisi

--- Ese1/src/ese1/DatiCondivisi.java
+++ Ese1/src/ese1/DatiCondivisi.java
@@ -5,10 +5,34 @@
  */
 package ese1;
 
+import java.util.concurrent.Semaphore;
+
 /**
  *
  * @author molteni_alessandro
  */
 public class DatiCondivisi {
+    private Semaphore arrivoR1;
+    private Semaphore arrivoR2;
     
+    public DatiCondivisi() {
+        arrivoR1 = new Semaphore(0);
+        arrivoR2 = new Semaphore(0);
+    }
+    
+    public Semaphore getArrivoR1() {
+        return arrivoR1;
+    }
+    
+    public Semaphore getArrivoR2() {
+        return arrivoR2;
+    }
+    
+    public void setArrivoR1(Semaphore arrivoR1) {
+        this.arrivoR1 = arrivoR1;
+    }
+    
+    public void setArrivoR2(Semaphore arrivoR2) {
+        this.arrivoR2 = arrivoR2;
+    }
 }
