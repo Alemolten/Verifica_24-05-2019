7866284602dd0fc2d3d78717d8d12449810519a6
molteni_alessandro <molteni_alessandro@INF1-21.jeanmonnet.local>
2019-05-24 11:50:08 +0200

    Creazione ThContaDispari

--- Ese2/src/ese2/ThContaDispari.java
+++ Ese2/src/ese2/ThContaDispari.java
@@ -5,10 +5,30 @@
  */
 package ese2;
 
+import java.util.*;
+
 /**
  *
  * @author molteni_alessandro
  */
 public class ThContaDispari {
+    DatiCondivisi ptrDati;
     
+    public ThContaDispari(DatiCondivisi ptrDati) {
+        this.ptrDati = ptrDati;
+    }
+    
+    public void run() {
+        int posizione = ptrDati.getPosizione();
+        ArrayList array = ptrDati.getNumeriGenerati();
+        Object numero = array.get(posizione);
+        int n = Integer.parseInt(numero.toString());
+        if(n%2==0) {
+        }
+        else {
+            int dispari = ptrDati.getNumeroLettiDispari();
+            dispari = dispari + 1;
+            ptrDati.setNumeroLettiDispari(dispari);
+        }
+    }
 }
